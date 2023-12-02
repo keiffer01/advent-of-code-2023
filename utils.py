c00d2224f88a3c91d9f46ee02878350b335d13b0
Keiffer Acoba <keiffer.acoba@gmail.com>
2023-12-02 04:26:51 -0500

    Change return type of read_input() to str.

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def _get_session_cookie() -> bytes:
 
 # Reads the puzzle input for the given day from the official 2023 Advent of Code
 # website. Uses my session cookie to log in.
-def read_input(day: int) -> bytes:
+def read_input(day: int) -> str:
     request = urllib.request.Request(
         "https://adventofcode.com/2023/day/{0}/input".format(day)
     )
