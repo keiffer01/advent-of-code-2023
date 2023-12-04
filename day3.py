9670af51ba6f2e2806a7054f3d083e5151479be9
Keiffer Acoba <keiffer.acoba@gmail.com>
2023-12-04 01:48:34 -0500

    Day 3 part 2 solution.

--- day3.py
+++ day3.py
@@ -15,6 +15,19 @@ def _is_valid_part_number(schematic: list[str], row: int,
         return True
     return False
 
+# Returns 0 if not a valid gear. Gear is located at (row, col).
+def _get_gear_ratio(schematic: list[str], row: int, col: int) -> int:
+    adjacent_nums = []
+    for r in range(max(0, row - 1), min(len(schematic), row + 2)):
+        for m in re.finditer(r"[0-9]+", schematic[r]):
+            if ((m.start() >= col - 1 and m.start() <= col + 1) or
+               (m.end() >= col and m.end() <= col + 2) or
+               (m.start() < col - 1 and m.end() > col + 2)):
+                adjacent_nums.append(m.group(0))
+    if len(adjacent_nums) == 2:
+        return int(adjacent_nums[0]) * int(adjacent_nums[1])
+    return 0
+
 def part1_solution():
     schematic = utils.read_input(day=3).splitlines()
     sum = 0
@@ -22,4 +35,12 @@ def part1_solution():
         for m in re.finditer(r"[0-9]+", line):
             if _is_valid_part_number(schematic, i, m):
                 sum += int(m.group(0))
+    return sum
+
+def part2_solution():
+    schematic = utils.read_input(day=3).splitlines()
+    sum = 0
+    for i, line in enumerate(schematic):
+        for m in re.finditer(r"\*", line):
+            sum += _get_gear_ratio(schematic, i, m.start())
     return sum
