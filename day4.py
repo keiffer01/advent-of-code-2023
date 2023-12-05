10f67cd0018ad23fb8e8ebc856d0637e6f4e98ef
Keiffer Acoba <keiffer.acoba@gmail.com>
2023-12-04 20:01:43 -0500

    Day 4 part 2 solution.

--- day4.py
+++ day4.py
@@ -1,13 +1,27 @@
 import utils
 
+# card should be in the format "Card _: 1 2 | 3 4 5".
+def _count_winning_numbers(card: str) -> int:
+    sets = card.replace("  ", " ").split(": ")[1].split(" | ")
+    winning_nums = {int(num) for num in sets[0].split(" ")}
+    my_nums = {int(num) for num in sets[1].split(" ")}
+    return len(winning_nums & my_nums)
+
+
 def part1_solution():
     cards: list[str] = utils.read_input(day=4).splitlines()
     sum = 0
     for card in cards:
-        sets = card.replace("  ", " ").split(": ")[1].split(" | ")
-        winning_nums = {int(num) for num in sets[0].split(" ")}
-        my_nums = {int(num) for num in sets[1].split(" ")}
-        power = len(winning_nums & my_nums)
+        power = _count_winning_numbers(card)
         if power > 0:
             sum += 2 ** (power - 1)
     return sum
+
+def part2_solution():
+    cards: list[str] = utils.read_input(day=4).splitlines()
+    card_counts = [1] * len(cards)
+    for i, card in enumerate(cards):
+        count = _count_winning_numbers(card)
+        for j in range(min(len(cards), i + 1), min(len(cards), i + 1 + count)):
+            card_counts[j] += card_counts[i]
+    return sum(card_counts)
