8c467e3f4039c416190e951440130fa9bbb4d6d0
Keiffer Acoba <keiffer.acoba@gmail.com>
2023-12-06 11:14:36 -0500

    Day 6 part 2 solution.

--- day6.py
+++ day6.py
@@ -15,3 +15,13 @@ def part1_solution():
                 count += 1
         results.append(count)
     return functools.reduce(lambda x, y: x * y, results, 1)
+
+def part2_solution():
+    input = re.sub(r" +", r"", utils.read_input(day=6)).splitlines()
+    time = int(input[0].split(":")[1])
+    distance = int(input[1].split(":")[1])
+    count = 0
+    for i in range(time + 1):
+        if i * (time - i) > distance:
+            count += 1
+    return count
