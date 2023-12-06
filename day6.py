0db7b9cfc5ef55d07a67bd60b0f24011ee138bc6
Keiffer Acoba <keiffer.acoba@gmail.com>
2023-12-06 11:11:34 -0500

    Day 6 part 1 solution.

---
+++ day6.py
@@ -0,0 +1,17 @@
+import functools
+import re
+import utils
+
+def part1_solution():
+    input = re.sub(r" +", r" ", utils.read_input(day=6)).splitlines()
+    times = [int(time) for time in input[0].split(": ")[1].split(" ")]
+    distances = [int(distance) for distance in 
+                 input[1].split(": ")[1].split(" ")]
+    results = []
+    for time, distance in zip(times, distances):
+        count = 0
+        for i in range(time + 1):
+            if i * (time - i) > distance:
+                count += 1
+        results.append(count)
+    return functools.reduce(lambda x, y: x * y, results, 1)
