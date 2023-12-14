2d48a24b95be627c0d4d4364e6acd153eb8c97b4
Keiffer Acoba <keiffer.acoba@gmail.com>
2023-12-14 17:01:31 -0500

    Day 9 part 2 solution.

--- day9.py
+++ day9.py
@@ -20,3 +20,20 @@ def part1_solution():
             values.append(values.pop() + values.pop())
         extrapolations.append(values[0])
     return sum(extrapolations)
+
+def part2_solution():
+    histories = _clean_input(utils.read_input(day=9))
+    extrapolations = []
+    for history in histories:
+        curr_history = history
+        values = [history[0]]
+        while not all([x == 0 for x in curr_history]):
+            new_history = []
+            for first, second in zip(curr_history, curr_history[1:]):
+                new_history.append(second - first)
+            values.append(new_history[0])
+            curr_history = new_history
+        while len(values) > 1:
+            values.append(-values.pop() + values.pop())
+        extrapolations.append(values[0])
+    return sum(extrapolations)
